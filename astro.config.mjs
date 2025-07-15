255a9301d5de2b2be16bc6261c30fddd0576457c
Florian Bertholin <arkan@users.noreply.github.com>
2025-07-15 14:52:27 +0200

    add remarkToc/remarkGfm

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 import { remarkReadingTime, remarkFillDescription } from './src/js/remark-reading-time.mjs';
+import remarkToc from 'remark-toc'
+import remarkGfm from 'remark-gfm'
 
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
-    remarkPlugins: [remarkReadingTime, remarkFillDescription],
+    remarkPlugins: [remarkReadingTime, remarkFillDescription, remarkToc, remarkGfm],
   },
   vite: {
     plugins: [tailwindcss()]
